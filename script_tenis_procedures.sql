910f74cd9df794c1cebb08985180521a90c0d126
AhmadMarPas <159173741+AhmadMarPas@users.noreply.github.com>
2025-03-06 16:40:40 +0100

    Incluida respuesta al paso 6.

--- script_tenis_procedures.sql
+++ script_tenis_procedures.sql
@@ -98,3 +98,47 @@ end;
 --2ª forma de ejecutar el procedimiento
 exec TEST_PROCEDURE_TENIS;
 /
+-- Paso 6.
+/*
+6.P.- Discute en un comentario de este último script si 2 transacciones ReservarPista se solapan en el tiempo queriendo ambas reservar la última pista que queda libre en una determinada fecha y hora y si sugieres algún arreglo al respecto. Implementa el arreglo en tal caso.
+7.R.- Sí, podría producirse un solapamiento: Podría darse el caso de que las 2 transacciones obtuvieran disponibilidad para realizar la reserva e intentaran realizar la inserción de la reserva.
+	En ese punto, sólo la primera en ser procesada podrá obtener la reserva y la segunda transacción obtendrá un error de clave duplicada.
+	Para solucionarlo, se puede utilizar la sentencia "SELECT FOR UPDATE".
+*/
+-- Implementación de la solución solicitada:
+CREATE OR REPLACE FUNCTION reservarPista(
+    p_socio VARCHAR,
+    p_fecha DATE,
+    p_hora INTEGER
+) RETURN INTEGER IS
+
+    CURSOR vPistasLibres IS
+        SELECT nro
+        FROM pistas 
+        WHERE nro NOT IN (
+            SELECT pista
+            FROM reservas
+            WHERE 
+                trunc(fecha) = trunc(p_fecha) AND
+                hora = p_hora
+        )
+        ORDER BY nro FOR UPDATE; -- Añadido "FOR UPDATE"
+
+    vPista INTEGER;
+
+BEGIN
+    OPEN vPistasLibres;
+    FETCH vPistasLibres INTO vPista;
+
+    IF vPistasLibres%NOTFOUND
+	THEN
+        CLOSE vPistasLibres;
+        RETURN 0;
+    END IF;
+
+    INSERT INTO reservas VALUES (vPista, p_fecha, p_hora, p_socio);
+    CLOSE vPistasLibres;
+    COMMIT;
+    RETURN 1;
+END;
+/
